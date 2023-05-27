8802b4f9a92962223a3d79419848c6a51deae3ea
jongphago <jonghyun.d.kim@gmail.com>
2023-05-27 18:43:58 +0900

    feat(plot.py): AIHub 데이터셋의 정확도를 시각화하는 함수 추가 AIHub 데이터셋의 정확도를 시각화하는 plot_accuracy_aihub 함수가 추가되었습니다. 이 함수는 LFW 데이터셋의 정확도를 시각화하는 plot_accuracy_lfw 함수와 유사하지만, AIHub 데이터셋에 대한 정확도를 시각화합니다.

--- plot.py
+++ plot.py
@@ -13,14 +13,18 @@ def plot_roc_lfw(false_positive_rate, true_positive_rate, figure_name="roc.png")
     roc_auc = auc(false_positive_rate, true_positive_rate)
     fig = plt.figure()
     plt.plot(
-        false_positive_rate, true_positive_rate, color='red', lw=2, label="ROC Curve (area = {:.4f})".format(roc_auc)
+        false_positive_rate,
+        true_positive_rate,
+        color="red",
+        lw=2,
+        label="ROC Curve (area = {:.4f})".format(roc_auc),
     )
     plt.plot([0, 1], [0, 1], color="blue", lw=2, linestyle="--", label="Random")
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Receiver Operating Characteristic (ROC) Curve")
     plt.legend(loc="lower right")
     fig.savefig(figure_name, dpi=fig.dpi)
     plt.close()
@@ -34,18 +38,49 @@ def plot_accuracy_lfw(log_file, epochs, dataset="LFW", figure_name="lfw_accuracy
         epochs (int): Number of training epochs finished.
         figure_name (str): Name of the image file of the resulting lfw accuracies plot.
     """
-    with open(log_file, 'r') as f:
+    with open(log_file, "r") as f:
         lines = f.readlines()
-        epoch_list = [int(line.split('\t')[0]) for line in lines]
-        accuracy_list = [round(float(line.split('\t')[1]), 2) for line in lines]
+        epoch_list = [int(line.split("\t")[0]) for line in lines]
+        accuracy_list = [round(float(line.split("\t")[1]), 2) for line in lines]
 
         fig = plt.figure()
-        plt.plot(epoch_list, accuracy_list, color='red', label=f'{dataset} Accuracy')
+        plt.plot(epoch_list, accuracy_list, color="red", label=f"{dataset} Accuracy")
         plt.ylim([0.0, 1.05])
         plt.xlim([0, epochs + 1])
-        plt.xlabel('Epoch')
-        plt.ylabel(f'{dataset} Accuracy')
-        plt.title(f'{dataset} Accuracies plot')
-        plt.legend(loc='lower right')
+        plt.xlabel("Epoch")
+        plt.ylabel(f"{dataset} Accuracy")
+        plt.title(f"{dataset} Accuracies plot")
+        plt.legend(loc="lower right")
+        fig.savefig(figure_name, dpi=fig.dpi)
+        plt.close()
+
+
+def plot_accuracy_aihub(
+    log_file, epochs, dataset="AIHub", figure_name="aihub_accuracy.png"
+):
+    """
+    AIHub 데이터셋의 정확도를 시각화하여 그래프로 저장합니다.
+
+    Parameters:
+    log_file (str): 로그 파일의 경로
+    epochs (int): 총 에포크 수
+    dataset (str, optional): 데이터셋의 이름 (기본값: "AIHub")
+    figure_name (str, optional): 저장할 그래프 이미지 파일의 이름 (기본값: "aihub_accuracy.png")
+
+    Returns:
+    None
+    """
+    with open(log_file, "r") as f:
+        lines = f.readlines()
+        # epoch_list = [int(line.split("\t")[0]) for line in lines]
+        accuracy_list = [round(float(line.split("\t")[1]), 2) for line in lines]
+        fig = plt.figure()
+        plt.plot(accuracy_list, color="red", label=f"{dataset} Accuracy")
+        plt.ylim([0.0, 1.05])
+        # plt.xlim([0, epochs + 1])
+        plt.xlabel("Epoch")
+        plt.ylabel(f"{dataset} Accuracy")
+        plt.title(f"{dataset} Accuracies plot")
+        plt.legend(loc="lower right")
         fig.savefig(figure_name, dpi=fig.dpi)
         plt.close()
